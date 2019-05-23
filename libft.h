fe8d553c0811277c6c05c51f048cfd533dba7bae
Emmanuel MANANA <emanana@c4r8s9.wethinkcode.co.za>
2019-05-23 18:03:22 +0200

    23may

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emanana <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 12:02:13 by emanana           #+#    #+#             */
-/*   Updated: 2019/05/22 16:28:45 by emanana          ###   ########.fr       */
+/*   Updated: 2019/05/23 16:03:59 by emanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,11 @@ int			ft_atoi(const char *str);
 int			ft_strlen(const char *str);
 char		*ft_strdup(const char *s1);
 char		*ft_strcpy(char *dst, const char *src);
-int			ft_strcmp(const char *s0, const char *s2);
-char		*ft_strncpy(char *dst, const char* src, size_t len);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+int			ft_strcmp(const char *s1, const char *s2);
+char		*ft_strncpy(char *dst, const char *src, size_t len);
+char		*ft_strcat(char *s1, const char *s2);
+char		*ft_strncat(char *s1, const char *s2, size_t n);
 
 #endif
