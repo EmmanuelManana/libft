ea5208854f6614471a3680c253da6672f4ee29da
Emmanuel MANANA <emanana@c4r8s9.wethinkcode.co.za>
2019-05-30 17:04:34 +0200

    30maydone with first part of llib ft

---
+++ ft_memcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emanana <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 16:22:23 by emanana           #+#    #+#             */
+/*   Updated: 2019/05/30 17:01:31 by emanana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int			ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char		*ps1;
+	unsigned char		*ps2;
+	int					i;
+
+	i = 0;
+	ps1 = (unsigned char*)s1;
+	ps2 = (unsigned char*)s2;
+	if (n == 0)
+		return (0);
+	while (*ps1 == *ps2 && i++ < n)
+	{
+		ps1++;
+		ps2++;
+	}
+	return ((int)(ps1 - ps2));
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emanana <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 12:02:13 by emanana           #+#    #+#             */
-/*   Updated: 2019/05/30 12:23:49 by emanana          ###   ########.fr       */
+/*   Updated: 2019/05/30 16:40:12 by emanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void		*ft_memmove(void *dts, const void *src, size_t len);
 void		*ft_memset(void	*b, int c, size_t len);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strncpy(char *dst, const char *src, size_t len);
