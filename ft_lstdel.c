6cac9cfd8fda2f08365c65658936ebed61cc2d69
Emmanuel MANANA <emanana@c4r8s6.wethinkcode.co.za>
2019-06-11 17:26:35 +0200

    commiting chagnes

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -14,15 +14,15 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	t_list	*ptr;
+	t_list	*node;
 	t_list	*tmp;
 
-	ptr = *alst;
-	while (ptr)
+	node = *alst;
+	while (node)
 	{
-		tmp = ptr;
-		ft_lstdelone(&ptr, del);
-		ptr = tmp->next;
+		tmp = node;
+		ft_lstdelone(&node, del);
+		node = tmp->next;
 	}
 	*alst = NULL;
 }
